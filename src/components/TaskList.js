22098462f483552080e96bb3e16ebfe06f1dddd1
Alex Carbajo <alexcarbajo@gmail.com>
2018-12-06 21:03:01 +0100

    Starting screens chapter

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Task from './Task';
 
-function TaskList({loading, tasks, onPinTask, onArchiveTask}) {
+import Task from './Task';
+import { connect } from 'react-redux';
+import { archiveTask, pinTask } from '../lib/redux';
+
+
+export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     const events = {
         onPinTask,
         onArchiveTask,
@@ -12,7 +16,9 @@ function TaskList({loading, tasks, onPinTask, onArchiveTask}) {
         <div className="loading-item">
             <span className="glow-checkbox"/>
             <span className="glow-text">
-                <span>Loading</span> <span>cool</span> <span>state</span>
+                <span>Loading</span>
+                <span>cool</span>
+                <span>state</span>
             </span>
         </div>
     );
@@ -54,16 +60,23 @@ function TaskList({loading, tasks, onPinTask, onArchiveTask}) {
     );
 }
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
     loading: PropTypes.bool,
     tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
     onPinTask: PropTypes.func.isRequired,
     onArchiveTask: PropTypes.func.isRequired,
 };
 
-TaskList.defaultProps = {
+PureTaskList.defaultProps = {
     loading: false,
 };
 
-export default TaskList;
-
+export default connect(
+    ({ tasks }) => ({
+        tasks: tasks.filter(t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'),
+    }),
+    dispatch => ({
+        onArchiveTask: id => dispatch(archiveTask(id)),
+        onPinTask: id => dispatch(pinTask(id)),
+    })
+)(PureTaskList);
